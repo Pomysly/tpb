48da5dc2e9993002c5e9106d2c7f5f64101bd348
ZofiaBialla <zofiabialla@gmail.com>
2014-07-06 20:33:18 +0200

    add last name and first name to registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :lastname
+    devise_parameter_sanitizer.for(:sign_up) << :firstname
+  end
+
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
