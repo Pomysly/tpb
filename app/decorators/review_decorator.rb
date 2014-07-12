bcb286682c4f4bd7a9bf9c95446f9f6c0799480a
ZofiaBialla <zofiabialla@gmail.com>
2014-07-12 17:57:06 +0200

    anonymous user can write review

--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -2,7 +2,13 @@ class ReviewDecorator < Draper::Decorator
   delegate_all
 
   def author  
+  	unless user == nil
   	self.review.user.firstname + " " + self.review.user.lastname 
+  else
+  	"anonymous"
+  	end
+
+
   end
 
 end
