59594f09b66ceae13758c55711b352afeedab8aa
ZofiaBialla <zofiabialla@gmail.com>
2014-07-12 15:09:54 +0200

    add parameter sanitizer for edit account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :lastname
-    devise_parameter_sanitizer.for(:sign_up) << :firstname
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) << :firstname
+    devise_parameter_sanitizer.for(:account_update) << :lastname
   end
-
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
